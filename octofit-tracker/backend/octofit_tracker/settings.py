0ea45b34d4a9f7b76bc91bb13da807f4acbf5205
Pratheeksha-99 <pratheeksha.pramod@accenture.com>
2026-03-31 09:25:31 +0000

    Setup Django REST Framework, start the server, and test the API

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,9 +25,11 @@ SECRET_KEY = 'django-insecure-p!sigoln)_!&sq#on0ulnck%ojv^pbdq!j$a-xj)aen^s(rlh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_name:
+    ALLOWED_HOSTS.append(f'{codespace_name}-8000.app.github.dev')
 
 
 # Application definition
@@ -45,6 +47,8 @@ INSTALLED_APPS = [
     'djongo',
     'corsheaders',
 ]
+
+AUTH_USER_MODEL = 'octofit_tracker.User'
 
 
 MIDDLEWARE = [
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -3,15 +3,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User, Team, Activity, Workout, Leaderboard
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, WorkoutSerializer, LeaderboardSerializer
+import os
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'users': '/api/users/',
-        'teams': '/api/teams/',
-        'activities': '/api/activities/',
-        'workouts': '/api/workouts/',
-        'leaderboard': '/api/leaderboard/',
+        'users': f'{base_url}/api/users/',
+        'teams': f'{base_url}/api/teams/',
+        'activities': f'{base_url}/api/activities/',
+        'workouts': f'{base_url}/api/workouts/',
+        'leaderboard': f'{base_url}/api/leaderboard/',
     })
 
 class UserViewSet(viewsets.ModelViewSet):
